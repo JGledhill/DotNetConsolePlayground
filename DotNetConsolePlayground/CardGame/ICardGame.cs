40017c754d4dfd91a891b6b302c41ee74b0a82cf
JGledhill <jack.gledhill@buttercups.co.uk>
2023-06-13 11:30:00 +0100

    Add interface comment

--- DotNetConsolePlayground/CardGame/ICardGame.cs
+++ DotNetConsolePlayground/CardGame/ICardGame.cs
@@ -2,6 +2,7 @@
 {
     /*
      * This is an interface that defines a contract for a class to implement.
+     * Naming convention is typically: I{ClassName}.
      * https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/types/interfaces
      */
     public interface ICardGame
